d33173038f7cd2415e884f90aba9190daf141e65
Gumbraise <vikiclashonyt@gmail.com>
2023-08-19 13:18:07 +0200

    ✨ Added update route on dev env

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\DeaddropRepository;
+use App\Service\UpdateDeaddrops;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,5 +18,14 @@ class HomepageController extends AbstractController
         return $this->render('homepage/index.html.twig', [
             'deaddrops' => $deaddropRepository->findAll(),
         ]);
+    }
+
+    #[Route('/update', name: 'app_update', env: 'dev')]
+    public function update(
+        UpdateDeaddrops $updateDeaddrops,
+    ): Response
+    {
+        $updateDeaddrops->update();
+        return new Response('Updated', Response::HTTP_OK);
     }
 }
